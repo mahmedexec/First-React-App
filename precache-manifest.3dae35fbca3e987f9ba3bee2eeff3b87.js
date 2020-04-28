4982956292c2dc85b7b594ac621fbfff5104f6e5
M <mahmedexec@gmail.com>
2020-04-29 02:05:19 +0500

    Updates

---
+++ precache-manifest.3dae35fbca3e987f9ba3bee2eeff3b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7051a9c50b8fd2974154888f47f076",
+    "url": "/mahmedexec/First-React-App/index.html"
+  },
+  {
+    "revision": "250a77de78894f9ade87",
+    "url": "/mahmedexec/First-React-App/static/css/main.12e0d9c4.chunk.css"
+  },
+  {
+    "revision": "0ed700e1c0316f5c057a",
+    "url": "/mahmedexec/First-React-App/static/js/2.f9d23888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mahmedexec/First-React-App/static/js/2.f9d23888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250a77de78894f9ade87",
+    "url": "/mahmedexec/First-React-App/static/js/main.a1e2ce17.chunk.js"
+  },
+  {
+    "revision": "b022f418651d22223a86",
+    "url": "/mahmedexec/First-React-App/static/js/runtime-main.79ce0196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mahmedexec/First-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
